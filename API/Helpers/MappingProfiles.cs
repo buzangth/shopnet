908e970c3511399c1d45d08abbf4b8a6ce230996
buzangth <thbusang@outlook.com>
2021-10-22 22:40:07 +0200

    automapper and resolver

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -14,7 +14,8 @@ namespace API.Helpers
         {
             CreateMap<Product,ProductDto>()
              .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-             .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+             .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+             .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductUrlResolver>());
         }
         
     }
